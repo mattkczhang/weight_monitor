80e76f8f831ab5ecc99f7a29badc9868d4b7c9d8
Matt-ZhangKaichong <31697843+Matt-ZhangKaichong@users.noreply.github.com>
2023-10-13 14:13:07 -0700

    second_draft

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import numpy as np
 import pandas as pd
 from datetime import date
 import streamlit as st
+import altair as alt
 
 # load data
 
 records = pd.read_csv('weight_record.csv')
+records.date = pd.to_datetime(records.date).dt.strftime('%Y-%m-%d')
+
 # records.drop(records.index, inplace=True)
 # records.to_csv('weight_record.csv', index=False)
 
@@ -14,19 +17,19 @@ records = pd.read_csv('weight_record.csv')
 def findDayofWeek(date):
     dow = date.today().weekday()
     if dow == 0:
-        return 'Monday'
+        return 'Monday 🚀'
     elif dow == 1:
-        return 'Tuesday'
+        return 'Tuesday 🌈'
     elif dow == 2:
-        return 'Wednesday'
+        return 'Wednesday ☕'
     elif dow == 3:
-        return 'Thursday'
+        return 'Thursday 😪'
     elif dow == 4:
-        return 'Friday'
+        return 'Friday 🎉'
     elif dow == 5:
-        return 'Saturday'
+        return 'Saturday 🍩'
     elif dow == 6:
-        return 'Sunday'
+        return 'Sunday 📚'
     
 def disable():
     st.session_state.button_disable = True
@@ -49,7 +52,7 @@ col1, col2 = st.columns(2)
 st.header("Happy "+todayDoW)
 enteredDate = str(st.date_input("Today's Date", date.today()))
 enteredName = st.selectbox(
-    'username',
+    'Username',
     ('Matt', 'Winnie'))
 enteredWeight = st.number_input("Today's weight in kg")
 if records.date.str.contains(enteredDate).any() and records[records.date==enteredDate].name.str.contains(enteredName).any():
@@ -62,14 +65,28 @@ submit = st.button(
 )
 
 if submit:
-    records = pd.concat([records, pd.DataFrame([[enteredDate,enteredName,enteredWeight]], columns=records.columns)], ignore_index=True)
+    print('-----------')
+    print(enteredDate)
+    temp =  pd.DataFrame([[enteredDate,enteredName,enteredWeight]], columns=records.columns)
+    records.date = pd.to_datetime(records.date).dt.strftime('%Y-%m-%d')
+    print(records.date)
+    records = pd.concat([records, temp], ignore_index=True)
     records.to_csv('weight_record.csv', index=False)
     st.experimental_rerun()
 
-    
+
+records.date = pd.to_datetime(records.date).dt.strftime('%Y-%m-%d')
+
 tab1, tab2 = st.tabs(["📈 Trend", "🗃 Data"])
 
-tab1.line_chart(data=records, x='date', y='weight', color='name')
+chart = alt.Chart(records).mark_line().encode(
+    alt.X('date').title('Time'),
+    alt.Y('weight',scale=alt.Scale(domain=[50, 80])).title('Weight in kg'),
+    color=alt.Color('name').title(" ").scale(domain=['Matt','Winnie'], range=['steelblue','pink'])
+)
+
+tab1.altair_chart(chart, use_container_width=True)
+
 
 tab2.write(records)
     
